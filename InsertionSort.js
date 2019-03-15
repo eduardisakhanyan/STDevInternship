5e9d836806d1412a6bac930f10e875bd57a78b29
Eduard Isakhanyan <isakhanyan.eduard@gmail.com>
2019-03-15 19:01:35 +0400

    Added break point

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,12 +1,13 @@
 function findPosition(arr, x , index) {
-    for (let i = index; i >= 0; --i) {
-        if (x < arr[i]) {
-            [arr[index], arr[i]] = [arr[i], arr[index]];
-        }    
+    for (let i = index - 1; i >= 0; --i) {
+        if (x >= arr[i]) {
+            break;
+        }
+        [arr[i+1], arr[i]] = [arr[i], arr[i+1]];    
     }
 }
 
-let array = [15, 2, 3, 8];
+let array = [1, 2, 3, 8, 1, 87];
 for (let i = 0; i < array.length; ++i) {
     findPosition(array, array[i] , i);
 }
